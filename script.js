07ba0ee9101df1dd386242bec8d0a6ae42bfa05c
Leonel Quintana <leonelquint@gmail.com>
2024-09-11 00:04:46 -0300

    git commit -m "modificaciones finalices al codigo html y css, mejora visualizacion codigo css"

--- script.js
+++ script.js
@@ -2,19 +2,16 @@ window.addEventListener("load", function() {
     const preloader = document.getElementById("preloader");
     const contenido = document.getElementById("contenido");
 
-    // Verifica si el preloader ya fue mostrado en esta sesión
     if (sessionStorage.getItem("preloaderShown")) {
-        preloader.style.display = "none"; // Oculta el preloader si ya se mostró en esta sesión
-        contenido.classList.remove("hidden"); // Muestra el contenido principal
-        document.body.style.overflow = "auto"; // Permite el scroll nuevamente
+        preloader.style.display = "none";
+        contenido.classList.remove("hidden");
+        document.body.style.overflow = "auto";
     } else {
-        // Si no se ha mostrado en esta sesión, asigna el evento de clic
         document.getElementById("logo").addEventListener("click", function() {
-            preloader.style.display = "none"; // Oculta el preloader al hacer clic
-            contenido.classList.remove("hidden"); // Muestra el contenido principal
-            document.body.style.overflow = "auto"; // Permite el scroll nuevamente
+            preloader.style.display = "none";
+            contenido.classList.remove("hidden");
+            document.body.style.overflow = "auto";
 
-            // Marca que el preloader ya fue mostrado en esta sesión
             sessionStorage.setItem("preloaderShown", "true");
         });
     }
